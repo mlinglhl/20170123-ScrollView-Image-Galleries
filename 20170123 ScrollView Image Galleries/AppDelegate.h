17e4634bcf3b9f4d27e784834726d922c4041eca
mlinglhl <minhung.ling@gmail.com>
2017-01-23 11:03:33 -0500

    Initial Commit

---
+++ 20170123 ScrollView Image Galleries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20170123 ScrollView Image Galleries
+//
+//  Created by Minhung Ling on 2017-01-23.
+//  Copyright © 2017 Minhung Ling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
